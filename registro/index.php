8c8c01c24d640ceac28d945b1ce667e0dc7294f2
cuellaralan <alan.javier.ajc@gmail.com>
2016-03-25 15:19:16 -0300

    formulario de registro basico

---
+++ registro/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pagina desde 0</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="">
+	</head>
+
+	<body>
+	<div id="envoltura">
+		
+		<div id="contenedor">
+			
+			<div id="cabecera">
+			
+				<div id="cuerpo">
+					<form>
+						
+						<label>Nombre de usuario: </label>
+						<input
+						id="nombre de usuario" name="Usuario" placeholder="ej: pepito66" autocomplete="off" autofocus="on">
+						</input>
+
+						<label>Contraseña: </label>
+						<input
+						id="contraseña" type="password" placeholder="ingrese contraseña">
+						</input>
+
+						<label>Repita Contraseña: </label>
+						<input
+						id="repita contraseña" type="password" placeholder="reingrese contraseña">
+						</input>
+
+						<label>E-mail: </label>
+						<input
+						id="correo" type="text" placeholder="ingrese e-mail">
+						</input>
+
+
+					</form>
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+
+
+
+	</body>
+
+</html>
