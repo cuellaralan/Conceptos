ca520f0804c6aac10520842d15f95c2f92dcc5c3
cuellaralan <alan.javier.ajc@gmail.com>
2016-03-25 15:38:25 -0300

    pagina home con botones login y registrar //falta funcionamiento de botones que redireccionen a los otros formularios

--- index.php
+++ index.php
@@ -1,50 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Pagina desde 0</title>
+		<title>Pagina de inicio</title>
 	</head>
+		<body>
 
-	<body>
-	<div id="envoltura">
-
-		<div id="contenedor">
-
-			<div id="cabecera">
-
-				<div id="cuerpo">
-					
-					<form id"form-login" action="" method="post" autocomplete="off">
-						<label>Usuario: </label>
-
-						<input
-						name="usuario" type="text" placeholder="ingrese usuario" autofocus="on" required="">
-							
-						</input>
-
-						<label>Contraseña: </label>
-						<input
-						name="contraseña" type="password" placeholder="ingrese contraseña" required="">	
-						</input>
-
-					</form>		
-
-
-				</div>
+			<form>
 				
-			</div>	
-
-		</div>
-		
-	</div>
+			<button type="submit">Login</button>
+			
+			<button type="submit">Registrarse</button>
 
 
-	
+			</form>
 
 
-	<?php
-
-	?>
-
-	</body>
-
+		</body>
 </html>
