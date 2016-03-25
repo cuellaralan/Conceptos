b3709b28804e2aa3da2c9a01817f8b90c9733dba
cuellaralan <alan.javier.ajc@gmail.com>
2016-03-25 14:56:16 -0300

    formulario sin estilos de login

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pagina desde 0</title>
+	</head>
+
+	<body>
+	<div id="envoltura">
+
+		<div id="contenedor">
+
+			<div id="cabecera">
+
+				<div id="cuerpo">
+					
+					<form id"form-login" action="" method="post" autocomplete="off">
+						<label>Usuario: </label>
+
+						<input
+						name="usuario" type="text" placeholder="ingrese usuario" autofocus="on" required="">
+							
+						</input>
+
+						<label>Contraseña: </label>
+						<input
+						name="contraseña" type="password" placeholder="ingrese contraseña" required="">	
+						</input>
+
+					</form>		
+
+
+				</div>
+				
+			</div>	
+
+		</div>
+		
+	</div>
+
+
+	
+
+
+	<?php
+
+	?>
+
+	</body>
+
+</html>
