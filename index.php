05e57ec23c238206ba23c6ec31ab7df4da9df7dc
cuellaralan <alan.javier.ajc@gmail.com>
2016-03-25 15:49:40 -0300

    Se agrego la funcionalidad a los botones login y registrar

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 
 			<form>
 				
-			<button type="submit">Login</button>
-			
-			<button type="submit">Registrarse</button>
+				<a href="./login/index.php" target="_self"> <input type="button" name="Login" value="Login" /> <!--- la etiqueta a define que el boton hace referencia a un enlace
+				href: direccion a acceder
+				target="_self" : Hace que el formulario se habra en la misma ventana
+				-->	
 
+				<a href="./registro/index.php" target="_self"><input type="button" name="registrar" value="registrar"/>
+				
+			
 
 			</form>
 
